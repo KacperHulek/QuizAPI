ab481ecc6d76ccecb41edb53b7b38cf0e9659602
Kacper <kacper2208@gmail.com>
2024-02-23 22:40:47 +0100

    Create service for creating quizzes

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,11 +1,93 @@
 import { Injectable } from '@nestjs/common';
 import { CreateQuizInput } from './dto/create-quiz.input';
 import { UpdateQuizInput } from './dto/update-quiz.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quiz } from './entities/quiz.entity';
+import { Question } from './entities/question.entity';
+import { TextAnswer } from './entities/text-answer.entity';
+import { Repository } from 'typeorm';
+import { PredefinedAnswer } from './entities/predefined-answer.entity';
+import { SortAnswer } from './entities/sort-answer.entity';
 
 @Injectable()
 export class QuizService {
-  create(createQuizInput: CreateQuizInput) {
-    return 'This action adds a new quiz';
+  constructor(
+    @InjectRepository(Quiz)
+    private quizzesRepository: Repository<Quiz>,
+    @InjectRepository(Question)
+    private questionsRepository: Repository<Question>,
+    @InjectRepository(TextAnswer)
+    private textAnswersRepository: Repository<TextAnswer>,
+    @InjectRepository(PredefinedAnswer)
+    private predefinedAnswersRepository: Repository<PredefinedAnswer>,
+    @InjectRepository(SortAnswer)
+    private sortAnswersRepository: Repository<SortAnswer>,
+  ) {}
+  async create(createQuizInput: CreateQuizInput): Promise<Quiz> {
+    const { name, questions: questionInputs } = createQuizInput;
+
+    const newQuiz = this.quizzesRepository.create({ name });
+    const savedQuiz = await this.quizzesRepository.save(newQuiz);
+
+    const createdQuestions = await Promise.all(
+      questionInputs.map(async (questionInput) => {
+        const { content, type, answers: answerInputs } = questionInput;
+        const newQuestion = this.questionsRepository.create({
+          content,
+          type,
+          quiz: savedQuiz,
+        });
+        const savedQuestion = await this.questionsRepository.save(newQuestion);
+
+        if (newQuestion.type === 'Predefined') {
+          const createdAnswers = await Promise.all(
+            answerInputs.map(async (answerInput) => {
+              const { content: answerContent, isCorrect } = answerInput;
+              let newAnswer;
+              newAnswer = this.predefinedAnswersRepository.create({
+                content: answerContent,
+                isCorrect,
+                question: savedQuestion,
+              });
+              return await this.predefinedAnswersRepository.save(newAnswer);
+            }),
+          );
+          savedQuestion.predefinedAnswers = createdAnswers;
+          return savedQuestion;
+        } else if (newQuestion.type === 'Sort') {
+          const createdAnswers = await Promise.all(
+            answerInputs.map(async (answerInput) => {
+              const { content: answerContent, sortOrder: order } = answerInput;
+              let newAnswer;
+              newAnswer = this.sortAnswersRepository.create({
+                content: answerContent,
+                order,
+                question: savedQuestion,
+              });
+              return await this.sortAnswersRepository.save(newAnswer);
+            }),
+          );
+          savedQuestion.predefinedAnswers = createdAnswers;
+          return savedQuestion;
+        } else if (newQuestion.type === 'Text') {
+          const createdAnswers = await Promise.all(
+            answerInputs.map(async (answerInput) => {
+              const { content: answerContent } = answerInput;
+              let newAnswer;
+              newAnswer = this.textAnswersRepository.create({
+                content: answerContent,
+                question: savedQuestion,
+              });
+              return await this.textAnswersRepository.save(newAnswer);
+            }),
+          );
+          savedQuestion.textAnswers = createdAnswers;
+          return savedQuestion;
+        }
+      }),
+    );
+    savedQuiz.questions = createdQuestions;
+    return savedQuiz;
   }
 
   findAll() {
