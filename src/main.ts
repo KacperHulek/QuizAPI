869676aa249015f3bdafaa1f86d0bc931a9bd873
Kacper <kacper2208@gmail.com>
2024-02-23 20:50:39 +0100

    Configure main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(
+      `Running API in MODE: ${process.env.NODE_ENV} on Port:${process.env.PORT}`,
+    );
+  });
 }
 bootstrap();
