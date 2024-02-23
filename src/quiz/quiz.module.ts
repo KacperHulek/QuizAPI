5fa16ba1124b290afa64a077c27cad2049bb5c49
Kacper <kacper2208@gmail.com>
2024-02-23 21:51:06 +0100

    Add to imports and exports necessary modules.

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizResolver } from './quiz.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quiz } from './entities/quiz.entity';
+import { Question } from './entities/question.entity';
+import { TextAnswer } from './entities/text-answer.entity';
+import { PredefinedAnswer } from './entities/predefined-answer.entity';
+import { SortAnswer } from './entities/sort-answer.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Quiz,
+      Question,
+      TextAnswer,
+      PredefinedAnswer,
+      SortAnswer,
+    ]),
+  ],
   providers: [QuizResolver, QuizService],
+  exports: [QuizService],
 })
 export class QuizModule {}
